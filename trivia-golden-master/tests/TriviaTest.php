be9d9536e4235b3fd193578a53394dc89f792733
Luis Rovirosa Mairlot <luis@codium.team>
2023-03-16 10:11:58 +0100

    [trivia] prepared for run phpunit tests

---
+++ trivia-golden-master/tests/TriviaTest.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class TriviaTest extends TestCase
+{
+    /** @test */
+    public function xxx(): void
+    {
+        $this->assertEquals(true, true);
+    }
+}
